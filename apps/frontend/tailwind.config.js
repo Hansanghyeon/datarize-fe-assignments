e4501d1d42150485ce9c1c0ef2db9028cb3aad67
Hansanghyeon <999@hyeon.pro>
2024-11-29 11:44:01 +0000

    fix: tailwind content css 누락 수정

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,12 +1,16 @@
+import daisyui from "daisyui"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,css}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui
+  ],
 }
 
