cb8390d06a378b379388cb59ea40e5a0374715fa
Hansanghyeon <999@hyeon.pro>
2024-11-30 15:19:51 +0000

    feat: daisyui 추가

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -1,15 +1,17 @@
-import type { Config } from "tailwindcss";
+import daisyui from 'daisyui'
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  darkMode: ['selector'],
+  content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         sans: [
           '"Inter"',
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
+          'ui-sans-serif',
+          'system-ui',
+          'sans-serif',
           '"Apple Color Emoji"',
           '"Segoe UI Emoji"',
           '"Segoe UI Symbol"',
@@ -18,5 +20,5 @@ export default {
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [daisyui],
+} satisfies Config
