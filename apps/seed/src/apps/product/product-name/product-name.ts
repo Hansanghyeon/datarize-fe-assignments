f88973e94a6421f08a7d0db0e301c77ea5b7a0ac
Hansanghyeon <999@hyeon.pro>
2024-11-30 19:20:19 +0000

    refactor: parseCsv 함수 분리

--- apps/seed/src/apps/product/product-name/product-name.ts
+++ apps/seed/src/apps/product/product-name/product-name.ts
@@ -1,6 +1,5 @@
-import csvParse from 'csv-simple-parser'
-
 import { __faker } from '~/libs/faker'
+import { parseCsvField } from '~/libs/read-file-parse-csv'
 
 /**
  * @description
@@ -11,29 +10,12 @@ export async function* getProductName() {
   type T_csvRow = {
     product_name: string
   }
-  // ====================================== example.csv 데이터 생성
-  const exampleFile = await Bun.file(`${__dirname}/example.csv`)
-    .text()
-    .catch(() => null)
 
-  if (exampleFile) {
-    const csv = csvParse(exampleFile, { header: true }) as T_csvRow[]
-    for (const row of csv) {
-      yield row.product_name
-    }
-  }
+  // ====================================== example.csv 데이터 생성
+  yield* parseCsvField<T_csvRow>(`${__dirname}/example.csv`, 'product_name')
 
   // ====================================== seed.csv 데이터 생성
-  const seedFile = await Bun.file(`${__dirname}/seed.csv`)
-    .text()
-    .catch(() => null)
-
-  if (seedFile) {
-    const seedData = csvParse(seedFile, { header: true }) as T_csvRow[]
-    for (const row of seedData) {
-      yield row.product_name
-    }
-  }
+  yield* parseCsvField<T_csvRow>(`${__dirname}/seed.csv`, 'product_name')
 
   // ====================================== faker 데이터 생성
   while (true) {
---
+++ apps/seed/src/libs/read-file-parse-csv.ts
@@ -0,0 +1,26 @@
+import csvParse from 'csv-simple-parser'
+
+/**
+ * @description
+ * CSV 파일에서 모든 필드 또는 특정 필드를 반환하는 제네릭 함수
+ * @template T CSV 데이터의 타입
+ * @param {string} filePath 파일 경로
+ * @param {K} [key] 반환할 필드의 키 (없으면 전체 행 반환)
+ * @returns {AsyncGenerator<T[keyof T]>} 전체 행 또는 선택된 필드의 값 반환
+ */
+export async function* parseCsvField<T>(
+  filePath: string,
+  key: keyof T, // 필드 키만 전달
+): AsyncGenerator<T[keyof T]> {
+  try {
+    const fileContent = await Bun.file(filePath).text()
+    if (fileContent) {
+      const csv = csvParse(fileContent, { header: true }) as T[]
+      for (const row of csv) {
+        yield row[key]
+      }
+    }
+  } catch (error) {
+    console.error(`Failed to read or parse ${filePath}:`, error)
+  }
+}
