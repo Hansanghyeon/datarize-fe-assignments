b99c5ce82961138da0b23ff24e5cf170b5065cce
Hansanghyeon <999@hyeon.pro>
2024-12-05 01:17:18 +0000

    feat: seed를 여러개 사용할수있도록 구조변경

--- apps/seed/src/apps/product/product-name/product-name.ts
+++ apps/seed/src/apps/product/product-name/product-name.ts
@@ -1,3 +1,8 @@
+import fg from 'fast-glob'
+import * as A from 'fp-ts/Array'
+import { flow, pipe } from 'fp-ts/function'
+import * as S from 'fp-ts/string'
+
 import { __faker } from '~/libs/faker'
 import { parseCsvField } from '~/libs/read-file-parse-csv'
 
@@ -14,8 +19,17 @@ export async function* getProductName() {
   // ====================================== example.csv 데이터 생성
   yield* parseCsvField<T_csvRow>(`${__dirname}/example.csv`, 'product_name')
 
-  // ====================================== seed.csv 데이터 생성
-  yield* parseCsvField<T_csvRow>(`${__dirname}/seed.csv`, 'product_name')
+  // ====================================== seed_{[1-9]{1,3}}.csv 데이터 생성
+  let files = await fg(`${__dirname}/seed*.csv`)
+  // files에 __dirname을 모두 제거한다.
+  files = pipe(
+    files,
+    A.map((e) => e.replace(`${__dirname}/`, '')),
+    A.sortBy([S.Ord]),
+  )
+  for (const file of files) {
+    yield* parseCsvField<T_csvRow>(`${__dirname}/${file}`, 'product_name')
+  }
 
   // ====================================== faker 데이터 생성
   while (true) {
