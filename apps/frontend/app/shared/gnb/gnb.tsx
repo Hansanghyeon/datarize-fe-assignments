b673cfc26c58256cf8c640cb13ca600e45926517
Hansanghyeon <999@hyeon.pro>
2024-12-07 09:11:25 +0000

    wip: gnb notification, clerk, theme toggle 적용

--- apps/frontend/app/shared/gnb/gnb.tsx
+++ apps/frontend/app/shared/gnb/gnb.tsx
@@ -1,20 +1,30 @@
+import { IconNotification } from '~/shared/components/icons/notification'
+import { ModeToggle } from '~/shared/components/mode-toggle'
+import { Popover, PopoverContent, PopoverTrigger } from '~/shared/components/ui/popover'
+import { NotificationPack } from '~/widget/notification'
+
+import { GnbMemoClerkNav } from './_partials/clerk'
+
 export function Gnb() {
   return (
-    <header>
-      <div className="container">
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
-        </nav>
+    <header className="border-b">
+      <div className="container py-3">
+        <div className="grid grid-cols-[200px,1fr,200px]">
+          <div>logo</div>
+          <div>center</div>
+          <div className="flex justify-end items-center gap-x-2">
+            <Popover>
+              <PopoverTrigger>
+                <IconNotification className="text-[14px]" />
+              </PopoverTrigger>
+              <PopoverContent align="end">
+                <NotificationPack />
+              </PopoverContent>
+            </Popover>
+            <GnbMemoClerkNav />
+            <ModeToggle />
+          </div>
+        </div>
       </div>
     </header>
   )
