e1f47dbfb323452c470bdc13a05a61c4c455cd46
Hansanghyeon <999@hyeon.pro>
2024-12-11 01:40:56 +0000

    feat(#18): gnb 요소 추가, 스타일 수정

--- apps/frontend/app/shared/gnb/gnb.tsx
+++ apps/frontend/app/shared/gnb/gnb.tsx
@@ -1,23 +1,36 @@
+import { HiOutlineLightBulb } from 'react-icons/hi2'
+import { Link } from 'react-router'
+
 import { IconNotification } from '~/shared/components/icons/notification'
 import { ModeToggle } from '~/shared/components/mode-toggle'
 import { Popover, PopoverContent, PopoverTrigger } from '~/shared/components/ui/popover'
 import { NotificationPack } from '~/widget/notification'
 
 import { Logo } from '../components/logo'
+import { badgeVariants } from '../components/ui/badge'
 import { Button } from '../components/ui/button'
+import { cn } from '../lib/utils'
 import { GnbMemoClerkNav } from './_partials/clerk'
 
 export function Gnb() {
   return (
     <header className="border-b">
-      <div className="container py-3">
-        <div className="grid grid-cols-[200px,1fr,200px]">
-          <div className="grid grid-cols-[auto,1fr] gap-x-2">
-            <div className="w-[48px] h-[48px] flex items-center justify-center bg-brand-200 rounded-xl border">
+      <div className="container">
+        <div className="grid grid-cols-[auto,1fr,200px] py-3">
+          <div className="flex gap-x-4 items-center">
+            <div className="w-[48px] h-[48px] flex items-center justify-center bg-brand-200 rounded-xl">
               <Logo className="text-[24px]" />
             </div>
-            <div className="self-center">
-              <b>쇼핑몰 구매 데이터</b>
+            <div className="flex gap-2 items-center">
+              <h2 className="typo-h2-sub font-semibold">쇼핑몰 구매 데이터</h2>
+              <Link
+                className={cn(badgeVariants({ variant: 'secondary' }), 'font-normal text-[10px] px-[6px] pl-[5px]')}
+                to="https://datarize.gitbook.io/docs"
+                target="_blank"
+              >
+                <HiOutlineLightBulb />
+                가이드 바로가기
+              </Link>
             </div>
           </div>
           <div className="self-center"></div>
